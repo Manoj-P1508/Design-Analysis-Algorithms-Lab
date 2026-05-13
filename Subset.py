c6a6991554d45849dedae9259e2a7b353b712efb
Manoj P <133762015+Manoj-P1508@users.noreply.github.com>
2026-05-13 10:08:11 +0530

    Update Subset.py

--- Subset.py
+++ Subset.py
@@ -1,16 +1,21 @@
 # Subset Sum
+found=False
 def subset_sum(s, target, subset, index):
+    global found
     if target == 0:                # If target becomes 0, subset is found
         print(subset)
+        found=True
         return
     
     for i in range(index, len(s)): # Traverse remaining elements
         if s[i] <= target:         # Include element only if it does not exceed target
             subset.append(s[i])    # Choose element
             subset_sum(s, target - s[i], subset, i + 1)     # Recur for remaining sum
-            subset.pop()    # Backtrack
+            subset.pop()                                    # Backtrack
 
 s = list(map(int, input("Enter set elements: ").split()))
 d = int(input("Enter target sum: "))
 print("Subsets with sum", d, "are:")
 subset_sum(s, d, [], 0)
+if not found:
+    print("NO possible solution")
